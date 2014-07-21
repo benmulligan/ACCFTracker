c0f69389bcad8eca99bd68a653554f63db511bc5
Ben Mulligan <benpmulligan@gmail.com>
2014-07-21 18:53:29 -0400

    Add update/delete to Fish table
    - Attempting to add a fish with a name that exists will now overwrite
    that fish instead
    - You can now delete fish
    - TODO: copy this functionality to bugs

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ACCFTracker
 ===========
 
 Bug/Fish tracker for AC:CF
+
+This is a simple project created for the purpose of learning AngularJS
--- cityfolk.js
+++ cityfolk.js
@@ -49,13 +49,48 @@
 		this.showAddBug = true;
 		
 		this.currentFish = [];
-		this.addFish = function(){
-			fishService.addFish(this.currentFish);
+		this.addFish = function()
+		{
+			var alreadyExists = false;
+			for (var i=0; i < this.allFish.length; i++)
+			{
+				if (this.allFish[i].name === this.currentFish.name)
+				{
+					// already exists, modify
+					alreadyExists=true;
+					this.allFish[i] = this.currentFish;
+					break;
+				}
+			}
+			if (!alreadyExists)
+			{
+				// new fish, create it
+				fishService.addFish(this.currentFish);
+			}
+			
 			this.currentFish = [];
 		}
 		
+		this.deleteFish = function(name)
+		{
+			for (var i=0; i < this.allFish.length; i++)
+			{
+				if (this.allFish[i].name === name)
+				{
+					// already exists, modify
+					alreadyExists=true;
+					this.allFish.splice(i, 1);
+					break;
+				}
+			}		
+		}
+		
 		this.currentBug = [];
-		this.addBug = function(){
+		this.addBug = function()
+		{
+			// TODO: If a bug by that name exists, update it
+			// Otherwise, create
+
 			bugService.addBug(this.currentBug);
 			this.currentBug = [];
 		}
